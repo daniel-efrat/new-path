a0524f9074f69e702f716f1f3c3cac86dfc16391
danielush <daniel989@gmail.com>
2026-06-05 17:11:52 +0200

    feat: add rewrites for shape asset paths in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   outputFileTracingRoot: __dirname,
+  async rewrites() {
+    return [
+      {
+        source: '/shapes/:path*',
+        destination: '/assets/questionnaire/shapes/:path*',
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
